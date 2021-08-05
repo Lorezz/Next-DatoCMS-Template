3ab2968999823f2835674b81a5ba4b2695c7dc3e
Lorezz <point.point@gmail.com>
2021-08-05 12:14:55 +0200

    mapbox sample

--- pages/samples/post.js
+++ pages/samples/post.js
@@ -1,10 +1,18 @@
-import { Container } from '@chakra-ui/react';
+import dynamic from 'next/dynamic'
+import { Container, Box } from '@chakra-ui/react';
 
-import Cta from 'components/samples/Cta';
+
+ import Cta from 'components/samples/Cta';
 import Post from 'components/samples/Post';
 import Newsletter from 'components/samples/Newsletter';
 import Gallery from 'components/samples/Gallery';
 import Layout from 'components/samples/Layout';
+
+const Map =  dynamic(() => import("components/maps/MapMapbox"), {ssr: false});
+
+//const MapWithNoSSR = dynamic(() => import("components/maps/MapLeaflet"), {ssr: false});
+//  const position = [42.97960654540092, 10.686549498402442]
+const position = {lat:42.97960654540092, lng:10.686549498402442, zoom:15 }
 
 const page = () => {
   return (
@@ -13,6 +21,7 @@ const page = () => {
         <Post />
         <Gallery />
         <Cta />
+        <Map name="RioTorto" position={position}/>
         <Newsletter />
       </Container>
     </Layout>
